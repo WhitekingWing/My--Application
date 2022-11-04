1ef4004fc8d1b0fd5c8378d6ee2bfae89e93cc0c
WhitekingWing <189642429@qq.com>
2022-11-05 00:24:06 +0800

    第三次修改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,55 +1,228 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import com.example.myapplication.data.DataSaver;
+import com.example.myapplication.data.ShopItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
-    TextView textView_HelloWorld;
-    Button button_Chinese;
-    Button button_English;
+
+    public static final int MENU_ID_ADD = 1;
+    public static final int MENU_ID_UPDATE = 2;
+    public static final int MENU_ID_DELETE = 3;
+    private ArrayList<ShopItem> shopItems;
+    private MainRecycleViewAdapter mainRecycleViewAdapter;
+
+
+    private ActivityResultLauncher<Intent> addDataLauncher= registerForActivityResult(new ActivityResultContracts.StartActivityForResult()
+            ,result -> {
+                if(null!=result){
+                    Intent intent=result.getData();
+                    if(result.getResultCode()==InputShopItemActivity.RESULT_CODE_SUCCESS)
+                    {
+                        Bundle bundle=intent.getExtras();
+                        String title= bundle.getString("title");
+                        double price=bundle.getDouble("price");
+                        int position=bundle.getInt("position");
+                        shopItems.add(position, new ShopItem(title,price,R.drawable.ic_launcher_background) );
+                        new DataSaver().Save(this,shopItems);
+                        mainRecycleViewAdapter.notifyItemInserted(position);
+                    }
+                }
+            });
+    private ActivityResultLauncher<Intent> updateDataLauncher= registerForActivityResult(new ActivityResultContracts.StartActivityForResult()
+            ,result -> {
+                if(null!=result){
+                    Intent intent=result.getData();
+                    if(result.getResultCode()==InputShopItemActivity.RESULT_CODE_SUCCESS)
+                    {
+                        Bundle bundle=intent.getExtras();
+                        String title= bundle.getString("title");
+                        double price=bundle.getDouble("price");
+                        int position=bundle.getInt("position");
+                        shopItems.get(position).setTitle(title);
+                        shopItems.get(position).setPrice(price);
+                        new DataSaver().Save(this,shopItems);
+                        mainRecycleViewAdapter.notifyItemChanged(position);
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        textView_HelloWorld = findViewById(R.id.textView);
-        button_Chinese = findViewById(R.id.button);
-        button_English = findViewById(R.id.button_english);
-//        androidx.constraintlayout.widget.ConstraintLayout layout = findViewById(R.id.button_chinese);
-//        layout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                textView_HelloWorld.setText("你好世界！");
-//            }
-//        });
-//        button_Chinese.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                textView_HelloWorld.setText("你好，世界！");
-//                //textView_HelloWorld.setText(R.string.string_chinese);
-//            }
-//        });
-//        button_English.setOnClickListener(new EnglishClickListener());
-        button_Chinese.setOnClickListener(new ButtonClickListener());
-        button_English.setOnClickListener(new ButtonClickListener());
+
+        RecyclerView recyclerViewMain=findViewById(R.id.recycleview_main);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerViewMain.setLayoutManager(linearLayoutManager);
+
+        DataSaver dataSaver=new DataSaver();
+        shopItems=dataSaver.Load(this);
+
+        if(shopItems.size()==0) {
+            shopItems.add(new ShopItem("item 0", Math.random() * 10, R.drawable.folder));
+        }
+        FloatingActionButton addBotton = findViewById(R.id.floatingActionButton);
+        addBotton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("position",0);
+                intent.setClass(MainActivity.this,InputShopItemActivity.class);
+                addDataLauncher.launch(intent);
+            }
+        });
+        mainRecycleViewAdapter= new MainRecycleViewAdapter(shopItems);
+        recyclerViewMain.setAdapter(mainRecycleViewAdapter);
     }
 
-    private class ButtonClickListener implements View.OnClickListener {
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case MENU_ID_ADD:
+                Intent intent=new Intent(this, InputShopItemActivity.class);
+                intent.putExtra("position",item.getOrder());
+                addDataLauncher.launch(intent);
+                break;
+            case MENU_ID_UPDATE:
+                Intent intentUpdate=new Intent(this, InputShopItemActivity.class);
+                intentUpdate.putExtra("position",item.getOrder());
+                intentUpdate.putExtra("title",shopItems.get(item.getOrder()).getTitle());
+                intentUpdate.putExtra("price",shopItems.get(item.getOrder()).getPrice());
+                updateDataLauncher.launch(intentUpdate);
+                break;
+            case MENU_ID_DELETE:
+                AlertDialog alertDialog;
+                alertDialog = new AlertDialog.Builder(this)
+                        .setTitle(R.string.confirmation)
+                        .setMessage(R.string.sure_to_delete)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                shopItems.remove(item.getOrder());
+                                new DataSaver().Save(MainActivity.this,shopItems);
+                                mainRecycleViewAdapter.notifyItemRemoved(item.getOrder());
+                            }
+                        }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+                alertDialog.show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    public class MainRecycleViewAdapter extends RecyclerView.Adapter<MainRecycleViewAdapter.ViewHolder> {
+
+        private ArrayList<ShopItem> localDataSet;
+
+        /**
+         * Provide a reference to the type of views that you are using
+         * (custom ViewHolder).
+         */
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
+            private final TextView textViewTitle;
+
+            public TextView getTextViewPrice() {
+                return textViewPrice;
+            }
+
+            private final TextView textViewPrice;
+            private final ImageView imageViewImage;
+
+            public ViewHolder(View view) {
+                super(view);
+                // Define click listener for the ViewHolder's View
+
+                imageViewImage = view.findViewById(R.id.imageview_item_image);
+                textViewTitle = view.findViewById(R.id.textview_item_caption);
+                textViewPrice = view.findViewById(R.id.textview_item_price);
+
+                view.setOnCreateContextMenuListener(this);
+            }
+
+            public TextView getTextViewTitle() {
+                return textViewTitle;
+            }
+
+            public ImageView getImageViewImage() {
+                return imageViewImage;
+            }
+
+            @Override
+            public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+                contextMenu.add(0,MENU_ID_ADD,getAdapterPosition(),"Add "+getAdapterPosition());
+                contextMenu.add(0,MENU_ID_UPDATE,getAdapterPosition(),"Update "+getAdapterPosition());
+                contextMenu.add(0,MENU_ID_DELETE,getAdapterPosition(),"Delete "+getAdapterPosition());
+            }
+        }
+
+        /**
+         * Initialize the dataset of the Adapter.
+         *
+         * @param dataSet String[] containing the data to populate views to be used
+         * by RecyclerView.
+         */
+        public MainRecycleViewAdapter(ArrayList<ShopItem> dataSet) {
+            localDataSet = dataSet;
+        }
+
+        // Create new views (invoked by the layout manager)
         @Override
-        public void onClick(View view) {
-            if(view == button_English) {
-                textView_HelloWorld.setText("Hello World!");
-                Log.v("test11",((Button)view).getText().toString());
-            }
-            else{
-                textView_HelloWorld.setText("你好，世界！");
-                Log.v("test232",((Button)view).getText().toString());
-            }
+        @NonNull
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new view, which defines the UI of the list item
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.item_main, viewGroup, false);
+
+            return new ViewHolder(view);
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+            // Get element from your dataset at this position and replace the
+            // contents of the view with that element
+            viewHolder.getTextViewTitle().setText(localDataSet.get(position).getTitle());
+            viewHolder.getTextViewPrice().setText(localDataSet.get(position).getPrice().toString());
+            viewHolder.getImageViewImage().setImageResource(localDataSet.get(position).getResourceId());
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return localDataSet.size();
         }
     }
 }
